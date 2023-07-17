3dfa78b61325581baa8bbc28429329ce407be612
MarkSant <santana.marco.a@gmail.com>
2023-07-17 13:34:00 -0300

    Start1

--- main.ts
+++ main.ts
@@ -1,137 +1,74 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin, TFile } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async onload() {
+    const filePath = 'C:\\Users\\santa\\OneDrive\\Programing\\Ob_Plug\\Papers\\My Library.bib';
 
-	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    try {
+      // Fetch the initial content
+      const initialContent = await this.fetchBibFileContent(filePath);
+      console.log('Initial content:', initialContent);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+      // Process the initial content
+      await this.processBibFileContent(initialContent);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+      // Subscribe to file change events
+      this.registerEvent(
+        this.app.vault.on('modify', async (file) => {
+          if (file.path === filePath) {
+            // Fetch the updated content
+            const updatedContent = await this.fetchBibFileContent(filePath);
+            console.log('Updated content:', updatedContent);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+            // Process the updated content
+            await this.processBibFileContent(updatedContent);
+          }
+        })
+      );
+    } catch (error) {
+      console.error('Error while fetching/processing content:', error);
+    }
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async fetchBibFileContent(filePath: string): Promise<string> {
+    const fileData = await this.app.vault.adapter.read(filePath);
+    return fileData;
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  async processBibFileContent(content: string): Promise<void> {
+    // Split content into individual entries based on the BibTeX entry delimiter
+    const entries = content.split('\n@');
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    for (const entry of entries) {
+      if (entry.trim() === '') continue; // Skip empty entries
 
-	onunload() {
+      // Extract the entry key from the first line
+      const entryKeyMatch = entry.match(/^article{(.+?),/);
+      if (!entryKeyMatch) continue; // Skip invalid entries
+      const entryKey = entryKeyMatch[1];
 
-	}
+      // Generate the file name based on the entry key
+      const fileName = `@${entryKey}.md`;
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+      // Generate the file path within the "Papers" folder
+      const filePath = `Papers/${fileName}`;
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+      // Check if the file already exists
+      const existingFile = this.app.vault.getAbstractFileByPath(filePath);
+      if (existingFile instanceof TFile) {
+        // File already exists, update its content
+        await this.app.vault.modify(existingFile, entry);
+        console.log('Updated file:', filePath);
+      } else {
+        // File doesn't exist, create a new file
+        await this.app.vault.create(filePath, '');
+        const file = this.app.vault.getAbstractFileByPath(filePath);
+        if (file instanceof TFile) {
+          await this.app.vault.modify(file, entry);
+          console.log('Created file:', filePath);
+        }
+      }
+    }
+  }
 }
